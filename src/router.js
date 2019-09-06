3a18f1c537e1d89d16ec144cc3f84d90425a3cb2
misato.uematsu <misato.uematsu@gpol.co.jp>
2019-09-06 18:41:44 +0900

    add

--- src/router.js
+++ src/router.js
@@ -6,6 +6,12 @@ import Home from '@/views/Home.vue'
 import Product from '@/views/Product.vue'
 import ProductList from '@/views/ProductList.vue'
 
+//Productの子コンポーネントたち
+
+import ProductHome from '@/views/Product/Home.vue'
+import ProductReview from '@/views/Product/Review.vue'
+import ProductReviewDetail from '@/views/Product/ReviewDetail.vue'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
